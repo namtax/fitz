d58cc672edb958fc92f7d7cfb7b2910395567cce
namtax <namtax@hotmail.com>
2016-01-19 16:32:42 +0000

    service_specs: mock out calls to postcode api

--- spec/services/fetch_coords_service_spec.rb
+++ spec/services/fetch_coords_service_spec.rb
@@ -1,14 +1,33 @@
 describe FetchCoordsService do
   subject { described_class }
   describe '#run' do
+    let(:connection) { double }
+    let(:response)   { double(code: code, body: body) }
+    let(:api_base)   { 'api.postcodes.io' }
+
+    before { allow(Net::HTTP).to receive(:new).with(api_base, 80).and_return(connection) }
+
     context 'valid postcode' do
+      before       { allow(connection).to receive(:get).with(path).and_return(response) }
+
+      let(:path)   { '/postcodes/E5%208HT' }
       let(:coords) { '51.5518497175874,-0.0532444526771821' }
+      let(:code)   { '200' }
+      let(:body)   { File.read(Rails.root.join('spec/fixtures/coords/success.json')) }
+
       it { expect(subject.run('E5 8HT').result).to eq (coords) }
     end
 
     context 'invalid postcode' do
-      let(:coords) { '51.5518497175874,-0.0532444526771821' }
+      before       { allow(connection).to receive(:get).with(path).and_return(response) }
+
+      let(:path)   { '/postcodes/E' }
+      let(:code)   { 400 }
+      let(:body)   { ''}
+
       it { expect(subject.run('E').errors).to eq (['Please supply a valid postcode']) }
     end
+
+    after { subject.instance_variable_set(:@connection, nil) }
   end
 end
--- spec/services/find_racecourses_service_spec.rb
+++ spec/services/find_racecourses_service_spec.rb
@@ -1,34 +1,46 @@
 describe FindRacecoursesService do
-  subject { described_class }
+  subject        { described_class }
+  let(:response) { double(errors: [], result: coords, valid?: true) }
+  let(:coords)   { '51.5518497175874,-0.0532444526771821' }
+
   describe '.run' do
-    it { expect(subject.run('E5 8HT').result.size).to eq 5 }
+    context 'success' do
+      it     { expect(subject.run('E5 8HT').result.size).to eq 5 }
 
-    context 'top result' do
-      let(:venue)   { subject.run('E5 8HT').result.first }
-      let(:address) { "Portsmouth Road, Esher, Surrey, KT10 9AJ, United Kingdom" }
+      before { expect(FetchCoordsService).to receive(:run).with('E5 8HT').and_return(response) }
 
-      it 'returns expected data' do
-        expect(venue.name).to eq 'Sandown Park Racecourse'
-        expect(venue.address).to eq address
+      context 'top result' do
+        let(:venue)    { subject.run('E5 8HT').result.first }
+        let(:address)  { "Portsmouth Road, Esher, Surrey, KT10 9AJ, United Kingdom" }
+
+        it 'returns expected data' do
+          expect(venue.name).to eq 'Sandown Park Racecourse'
+          expect(venue.address).to eq address
+        end
+      end
+
+      context 'second result' do
+        let(:venue)   { subject.run('E5 8HT').result[1] }
+        let(:address) { "75 High Holborn, London, Greater London, WC1V 6LS., United Kingdom" }
+
+        it 'returns expected data' do
+          expect(venue.name).to eq 'Racecourse Association'
+          expect(venue.address).to eq address
+        end
       end
     end
 
-    context 'second result' do
-      let(:venue)   { subject.run('E5 8HT').result[1] }
-      let(:address) { "75 High Holborn, London, Greater London, WC1V 6LS., United Kingdom" }
-
-      it 'returns expected data' do
-        expect(venue.name).to eq 'Racecourse Association'
-        expect(venue.address).to eq address
+    context 'errors' do
+      context 'postcode missing' do
+        it { expect(subject.run('').errors).to eq ['Please supply a valid postcode'] }
       end
-    end
 
-    context 'postcode missing' do
-      it { expect(subject.run('').errors).to eq ['Please supply a valid postcode'] }
-    end
+      context 'invalid postcode' do
+        let(:response) { double(errors: [], result: '', valid?: false)  }
 
-    context 'invalid postcode' do
-      it { expect(subject.run('e').errors).to eq ['Please supply a valid postcode'] }
+        before { expect(FetchCoordsService).to receive(:run).with('e').and_return(response) }
+        it     { expect(subject.run('e').errors).to eq ['Please supply a valid postcode'] }
+      end
     end
   end
 end
